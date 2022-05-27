3929bbd826e48a1ace1c8e5ccc38b5f3f89b4423
vale789092 <valeska2110@estudiantec.cr>
2022-05-26 23:37:49 -0600

    Update Aliado.java

--- View/Aliado.java
+++ View/Aliado.java
@@ -1,5 +1,21 @@
 package View;
+import java.awt.Color;
 
 public class Aliado {
-    
+    int fila;
+    int columna;
+
+    public Aliado(int i){
+        fila = (int) (Math.random()*(34-1)) + 1;
+        columna = (int) (Math.random()*(34-1)) + 1;
+        pintar(i);
+    }
+
+    public void pintar(int i){
+        while(fila == 17 && columna == 17){
+            fila = (int) (Math.random()*(34-1)) + 1;
+            columna = (int) (Math.random()*(34-1)) + 1;
+        }
+        View.bMatriz[fila][columna].setBackground(Color.green);
+    }
 }
