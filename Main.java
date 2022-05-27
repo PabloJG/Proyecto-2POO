9041fe2e5cf224a30c95db34bc5b04682887d9df
vale789092 <valeska2110@estudiantec.cr>
2022-05-26 23:36:35 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-import View.Enemigo;
-import View.Personaje;
-import View.View;
+import View.*;
+import Model.*;
+import Controller.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         View matriz = new View();
         Personaje pers = new Personaje();
-        Enemigo[] enemigos = new Enemigo[3];
-        for(int i = 0; i<3; i++){
-            enemigos[i] = new Enemigo(i);
-        }
+        Factory fact = new Factory();
     }
 }
