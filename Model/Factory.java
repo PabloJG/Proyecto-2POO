64b11495b57a9d90f156e80e23b254b59d48b543
vale789092 <valeska2110@estudiantec.cr>
2022-05-26 23:36:58 -0600

    Update Factory.java

--- Model/Factory.java
+++ Model/Factory.java
@@ -1,4 +1,28 @@
 package Model;
+import View.Aliado;
+import View.Enemigo;
+
 public class Factory{
-    
+    public static Enemigo[] enemigos;
+    Aliado[] aliados;
+
+    public Factory (){
+        enemigos = new Enemigo[100];
+        aliados = new Aliado[3];
+        iniciaralia();
+        iniciarenem();
+    }
+
+    public void iniciaralia(){
+        for(int i = 0; i<3; i++){
+            aliados[i] = new Aliado(i);
+        }
+    }
+
+    public void iniciarenem(){
+        for(int i = 0; i<3; i++){
+            enemigos[i] = new Enemigo(i);
+        }
+    }
+        
 }
