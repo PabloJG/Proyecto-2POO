2da77ee3aa3cde0dbe03cf7d4d84ef39c8189492
vale789092 <valeska2110@estudiantec.cr>
2022-05-31 13:16:08 -0600

    Update Factory.java

--- Model/Factory.java
+++ Model/Factory.java
@@ -6,18 +6,16 @@ import java.util.ArrayList;
 public class Factory{
     //public static Enemigo[] enemigos;
     public static ArrayList<Enemigo> enemigos = new ArrayList<Enemigo>();
-    Aliado[] aliados;
+    public static ArrayList<Aliado> aliados = new ArrayList<Aliado>();
 
     public Factory (){
-        //enemigos = new Enemigo[100];
-        aliados = new Aliado[3];
         iniciaralia();
         iniciarenem();
     }
 
     public void iniciaralia(){
         for(int i = 0; i<3; i++){
-            aliados[i] = new Aliado(i);
+            aliados.add(new Aliado());
         }
     }
 
