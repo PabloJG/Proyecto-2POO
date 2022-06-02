c3febfa5e4fa85322ff0be5a0683500799e5a77d
vale789092 <valeska2110@estudiantec.cr>
2022-06-01 18:53:25 -0600

    Update Controller.java

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,6 +1,63 @@
 package Controller;
-import View.View;
+import java.util.ArrayList;
+
+import Model.*;
+import View.*;
 
 public class Controller {
-    private View view;
+    private View matriz;
+    Enemigo enemigo;
+    Aliado aliado;
+    Personaje pers;
+    Model modelo;
+    Factory fact;
+    ArrayList<Enemigo> enemigos;
+
+    public void setMatriz(View matriz) {
+        this.matriz = matriz; 
+    }
+
+    public void setPersonaje(Personaje pers) {
+        this.pers = pers;
+    }
+
+    public void setModelo(Model modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setFactory(Factory fact) {
+        this.fact = fact;
+    }
+
+    public void disparo(int X, int Y, String posicion){
+        modelo.disparo(X, Y, posicion);
+    }
+
+    public boolean agregarnuevoE(int cant){
+        return fact.agregarnuevoE(cant);
+    }
+
+    public boolean agregarnuevoA(int cant){
+        return fact.agregarnuevoA(cant);
+    }
+
+    public void eliminarE(int x, int i) {
+        modelo.eliminarE(x, i);
+    }
+
+    void eliminarA(int fila, int columna){
+        modelo.eliminarA(fila, columna);
+    }
+
+    public boolean sumarVida(int fila, int columna){
+        return modelo.sumarVida(fila, columna);
+    }
+
+    public boolean restarVida(int fila, int columna){
+        return modelo.restarVida(fila, columna);
+    }
+
+    public void moverE(int[] coordp){
+        modelo.moverE(coordp);
+    }
 }
