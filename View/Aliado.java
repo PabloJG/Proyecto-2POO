3266b9b0712b7db61db6c7ea11bfc6c2a132df0e
vale789092 <valeska2110@estudiantec.cr>
2022-06-02 20:54:23 -0600

    Update Aliado.java

--- View/Aliado.java
+++ View/Aliado.java
@@ -1,18 +1,22 @@
 package View;
 import Model.Factory;
 import java.awt.Color;
+import java.util.Observable;
+import java.util.Observer;
 
 import Controller.Controller;
 
-public class Aliado implements TipoA{
-    Controller controlador;
+public class Aliado implements Observer{
+    Personaje personaje;
     int fila;
     int columna;
     public static int cantidadA;
-    int coordmov;
+    public static int coordmov;
     public int[] coord;
 
-    public Aliado(){
+    public Aliado(Personaje pers){
+        personaje = pers;
+        
         fila = (int) (Math.random()*(34-1)) + 1;
         columna = (int) (Math.random()*(34-1)) + 1;
         coord = new int[2];
@@ -23,12 +27,20 @@ public class Aliado implements TipoA{
     }
 
     @Override
-    public void tipo(){
+    public void update(Observable o, Object ag){
+        //
+    }
 
+    public static void agregarObservers(Personaje pers){
+        for(int i = 0; i < cantidadA; i++)
+            pers.addObserver(Factory.enemigos.get(i));
+    }
+
+    public static void colorear(int i){
+        View.bMatriz[Factory.enemigos.get(i).fila][Factory.enemigos.get(i).columna].setBackground(Color.green);
     }
 
     public void generar(){
-        System.out.println(cantidadA);
         if(cantidadA < 10){
             while(fila == 17 && columna == 17){
                 fila = (int) (Math.random()*(34-1)) + 1;
@@ -36,7 +48,8 @@ public class Aliado implements TipoA{
                 coord[0] = fila;
                 coord[1] = columna;
             }
-            View.bMatriz[coord[0]][coord[1]].setBackground(Color.green);
+            System.out.println(coord[0] + " "+ coord[1]);
+            //View.bMatriz[coord[0]][coord[1]].setBackground(Color.green);
             cantidadA++;
         }
     }
@@ -45,9 +58,5 @@ public class Aliado implements TipoA{
         for(int i = 0; i < cantidadA; i++){
             View.bMatriz[Factory.aliados.get(i).coord[0]][Factory.aliados.get(i).coord[1]].setBackground(Color.green);
         }
-    }
-
-    public void setCoordinador(Controller controlador) {
-        this.controlador = controlador;
     }
 }
