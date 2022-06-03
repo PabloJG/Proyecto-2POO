f0439ad8484d56717016a74eb18b14ebb4228b4d
vale789092 <valeska2110@estudiantec.cr>
2022-06-03 00:04:24 -0600

    Update Aliado.java

--- View/Aliado.java
+++ View/Aliado.java
@@ -1,22 +1,19 @@
 package View;
-import Model.Factory;
 import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
-
-import Controller.Controller;
+import Model.Factory;
 
 public class Aliado implements Observer{
-    Personaje personaje;
-    int fila;
-    int columna;
+    private Personaje personaje;
+    private int fila;
+    private int columna;
     public static int cantidadA;
     public static int coordmov;
     public int[] coord;
 
     public Aliado(Personaje pers){
         personaje = pers;
-        
         fila = (int) (Math.random()*(34-1)) + 1;
         columna = (int) (Math.random()*(34-1)) + 1;
         coord = new int[2];
@@ -28,19 +25,19 @@ public class Aliado implements Observer{
 
     @Override
     public void update(Observable o, Object ag){
-        //
+        System.out.println("entra aqui");
     }
 
     public static void agregarObservers(Personaje pers){
         for(int i = 0; i < cantidadA; i++)
-            pers.addObserver(Factory.enemigos.get(i));
+            pers.addObserver(Factory.aliados.get(i));
     }
 
     public static void colorear(int i){
-        View.bMatriz[Factory.enemigos.get(i).fila][Factory.enemigos.get(i).columna].setBackground(Color.green);
+        View.bMatriz[Factory.aliados.get(i).fila][Factory.aliados.get(i).columna].setBackground(Color.green);
     }
 
-    public void generar(){
+    private void generar(){
         if(cantidadA < 10){
             while(fila == 17 && columna == 17){
                 fila = (int) (Math.random()*(34-1)) + 1;
@@ -54,7 +51,7 @@ public class Aliado implements Observer{
         }
     }
 
-    public static void pintarTodos(){
+    static void pintarTodos(){
         for(int i = 0; i < cantidadA; i++){
             View.bMatriz[Factory.aliados.get(i).coord[0]][Factory.aliados.get(i).coord[1]].setBackground(Color.green);
         }
