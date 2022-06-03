7c432bdaf4d813f7750d9288a72b3941ca6738be
vale789092 <valeska2110@estudiantec.cr>
2022-06-02 20:53:10 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Controller controlador = new Controller();
         View matriz = new View();
-        Personaje pers = new Personaje();
         Model modelo = new Model();
-        Factory fact = new Factory();
+        Personaje pers = new Personaje();
+        Factory fact = new Factory(pers);
 
         matriz.setCoordinador(controlador);
         pers.setCoordinador(controlador);
@@ -19,5 +19,6 @@ public class Main {
         controlador.setPersonaje(pers);
         controlador.setModelo(modelo);
         controlador.setFactory(fact);
+
     }
 }
