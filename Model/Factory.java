6ec5eab5f4a10bf99d7e5fbe94fc3fecb2f8ad15
vale789092 <valeska2110@estudiantec.cr>
2022-06-02 20:53:37 -0600

    Update Factory.java

--- Model/Factory.java
+++ Model/Factory.java
@@ -6,40 +6,55 @@ import Controller.Controller;
 
 public class Factory{
     Controller controlador;
+    Personaje personaje;
     public static ArrayList<Enemigo> enemigos = new ArrayList<Enemigo>();
-    public static ArrayList<Aliado> aliados = new ArrayList<Aliado>();
+    public static ArrayList<Aliado> aliados = new ArrayList<Aliado>();  
 
-    public Factory (){
-        
+    public Factory(Personaje pers){
+        personaje = pers;
+    }
+
+    void nuevoA(){
+        aliados.add(new Aliado(personaje));
+        Aliado.agregarObservers(personaje);
+    }
+
+    void nuevoE(){
+        enemigos.add(new Enemigo(personaje));
+        Enemigo.agregarObservers(personaje);
     }
 
     public void iniciaralia(){
         for(int i = 0; i<3; i++){
-            aliados.add(new Aliado());
+            nuevoA();
         }
     }
 
     public void iniciarenem(){
         for(int i = 0; i<3; i++){
-            enemigos.add(new Enemigo());
+            nuevoE();
         }
+    }
+
+    void agregarObs(){
+        Enemigo.agregarObservers(personaje);
     }
 
     public void setCoordinador(Controller controlador) {
         this.controlador = controlador;
     }
 
-    public static boolean agregarnuevoE(int cant){
+    public boolean agregarE(int cant){
         if(cant == 9){
-            Factory.enemigos.add(new Enemigo());
+            nuevoE();
             return true;
         }
         return false;
     }
         
-    public static boolean agregarnuevoA(int cant){
+    public boolean agregarA(int cant){
         if(cant == 9){
-            Factory.aliados.add(new Aliado());
+            nuevoA();
             return true;
         }
         return false;
